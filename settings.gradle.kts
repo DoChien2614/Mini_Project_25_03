3399fd06d7fc614c2ea5b527b1e267bf359d1282
Anh Nguyen <nguyenhoanganh.nd12@gmail.com>
2026-03-25 15:06:04 +0700

    Add files via upload

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,9 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -19,5 +22,5 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Mini_Project_25_03"
+rootProject.name = "25thang3"
 include(":app")
